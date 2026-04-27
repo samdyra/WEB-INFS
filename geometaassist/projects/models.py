fc0b44b56174c6c2a54ffce75a3f637890d47561
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2026-04-27 23:23:32 +1000

    feat: project model

---
+++ geometaassist/projects/models.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.db import models
+
+
+class GeoDataProject(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['-updated_at']
+
+    def __str__(self):
+        return self.name
