2cf1839e387a9d708ba64d3c0189f62904178f03
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2026-04-29 23:58:12 +1000

    feat: geojson urls

--- geometaassist/projects/urls.py
+++ geometaassist/projects/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('<int:pk>/', views.project_detail, name='project_detail'),
     path('<int:pk>/edit/', views.project_edit, name='project_edit'),
     path('<int:pk>/delete/', views.project_delete, name='project_delete'),
+    path('<int:project_pk>/upload/', views.upload_geojson, name='upload_geojson'),
+    path('<int:project_pk>/uploads/<int:upload_pk>/', views.upload_detail, name='upload_detail'),
+    path('<int:project_pk>/uploads/<int:upload_pk>/delete/', views.upload_delete, name='upload_delete'),
 ]
