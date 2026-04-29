a849e90ceba54d1f8939c5cc70d0b39137e8ec0b
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2026-04-29 23:56:31 +1000

    feat: admin for geojson upload

--- geometaassist/projects/admin.py
+++ geometaassist/projects/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import GeoDataProject
+from .models import GeoDataProject, GeoJSONUpload
 
 
 @admin.register(GeoDataProject)
@@ -8,3 +8,11 @@ class GeoDataProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'user', 'created_at', 'updated_at', 'is_deleted')
     list_filter = ('is_deleted',)
     search_fields = ('name', 'user__email')
+
+
+@admin.register(GeoJSONUpload)
+class GeoJSONUploadAdmin(admin.ModelAdmin):
+    list_display = ('original_filename', 'project', 'upload_status',
+                    'file_size_bytes', 'uploaded_at', 'is_deleted')
+    list_filter = ('upload_status', 'is_deleted')
+    search_fields = ('original_filename', 'project__name')
