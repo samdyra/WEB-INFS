33978f737ddad4e3cbd67c2c42e1ae51f7561aec
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2026-05-03 15:34:58 +1000

    feat: urls for metadata

---
+++ geometaassist/metadata/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path(
+        'metadata/<int:project_pk>/uploads/<int:upload_pk>/retry/',
+        views.retry_extraction,
+        name='retry_extraction',
+    ),
+    path(
+        'metadata/<int:project_pk>/uploads/<int:upload_pk>/edit/',
+        views.metadata_editor,
+        name='metadata_editor',
+    ),
+    path(
+        'metadata/<int:project_pk>/uploads/<int:upload_pk>/export/',
+        views.export_stac,
+        name='export_stac',
+    ),
+    path(
+        'metadata/<int:project_pk>/uploads/<int:upload_pk>/export/success/',
+        views.export_success,
+        name='export_success',
+    ),
+    path(
+        'metadata/<int:project_pk>/uploads/<int:upload_pk>/download/',
+        views.download_stac,
+        name='download_stac',
+    ),
+]
